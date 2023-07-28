adec0368ce9c070848a0add78962917bec165ac4
Alcides Yunier Turruellas Osorio <ayunierto@gmail.com>
2023-07-28 07:12:34 -0500

    var types

--- app.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const msg = 'Hello World';
-const hero = {
-    name: "Iroman",
-    age: 45
-};
-hero.age = 50;
-console.log(hero.age);
---
+++ app.old.ts
@@ -0,0 +1,57 @@
+(() => {
+
+  const a:number = 10;
+
+  console.log( a );
+
+  let name:string = 'John Doe';
+  console.log(name);
+
+  let world = 'world';
+  console.log( ( world as string ).charAt(0));
+  
+  let age = 23;
+  console.log( (<number>age).toFixed(2) );
+  
+  let isActive:boolean = false;
+  console.log( isActive );
+  
+  // const numbers: (string | number | boolean)[] = [ 1, 2, 3, 4, '5', 6 ];
+  const numbers:number[] = [ 1, 2, 3, 4, 5, 6 ];
+  console.log( numbers );
+
+  // Tuples
+  const hero: [string, number] = ['Dr Strnge', 100];
+  hero[0] = 50;
+
+  // Enums
+  enum AudioLevel {
+    min,
+    medium,
+    max
+  } 
+
+  let currentLevel = AudioLevel.medium;
+  console.log(currentLevel);
+  
+  // Never
+  const asd = (message:string):never => {
+    throw new Error( message )
+  } 
+
+  asd('Helpme!!!');
+
+  // Null and undefined
+  let nothing:undefined = undefined;
+
+  console.log(nothing);
+
+  
+  
+
+
+
+
+
+
+})();
